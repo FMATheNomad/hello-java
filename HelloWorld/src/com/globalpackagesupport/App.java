a69e0877a340c7ad51e6c5998d40ee8e6512b499
FMA <111466154+FMATheNomad@users.noreply.github.com>
2023-06-16 01:02:37 +0700

    add

---
+++ HelloWorld/src/com/globalpackagesupport/App.java
@@ -0,0 +1,6 @@
+package com.globalpackagesupport;
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ HelloWorld/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author AGATHON
+ *
+ */
+module HelloWorld {
+}
